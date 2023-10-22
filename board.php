6959582da82e36c766ebf5c95176a984297237bd
Eric Li <lieric021@gmail.com>
2023-10-22 17:54:11 -0400

    started grid

--- board.php
+++ board.php
@@ -16,7 +16,7 @@
             <div class="col-xs-12">
             <h1>PHP Connections</h1>
             <h4>Hello <?=$name?>! (<?=$email?>)</h4>
-            <h4>Score: <?=$score?></h4>
+            <h4>Guesses: <?=$guess?></h4>
             </div>
         </div>
         <div class="row">
@@ -26,7 +26,45 @@
                     Words
                 </div>
                 <div class="card-body">
-                    <h5 class="card-title"><?=$question["question"]?></h5>
+                <?php
+
+                    $student_one = array("Food"=>"C1", "Food2"=>"C1", "Food3"=>"C1", "Food4"=>"C1",
+                        "Drinks"=>"C2", "Drinks2"=>"C2", "Drinks3"=>"C2", "Drinks4"=>"C2",
+                        "Plate"=>"C3", "Plate2"=>"C3", "Plate3"=>"C3", "Plate4"=>"C3",
+                        "Cup"=>"C4", "Cup2"=>"C4", "Cup3"=>"C4", "Cup4"=>"C4");
+
+                    shuffle($student_one);
+
+                    $html = "";
+
+                    $count=0;
+
+                    while($count<count($student_one)){
+                        $html .= "<div class=\"row\">\n"
+                        for($x = 0; $x<4; $x++){
+                            $html .="\t<div class=\"col\">\n
+                            \t\t <label>"+$student_one"</label>"
+                            $count++;
+                        }
+                    }
+
+                    foreach($ar_rows as $row)
+                    {
+                    $html .= "<tr><th>".$row."</th>\n";
+
+                    foreach($ar_columns as $col)
+                    {
+                        $cell_str = (in_array($col.$row, $ar_addresses) ? "match" : "&nbsp;");
+                        $html .= "<td>".$cell_str."</td>\n";
+                    }
+
+                    $html .= "</tr>\n";
+                    }
+
+                    $html .= "</table>\n";
+
+                    echo $html;
+                ?>
                 </div>
             </div>
             </div>
@@ -34,20 +72,10 @@
             <div class="row">
                 <div class="col-xs-12">
                 <form action="?command=answer" method="post">
-                    <input type="hidden" name="questionid" value="<?=$question["id"]?>">
-
                     <div class="mb-3">
-                        <label for="answer" class="form-label">Trivia Answer: </label>
-                        <input type="text" class="form-control" id="trivia-answer" name="answer">
+                        <label for="answer" class="form-label">Guess: </label>
+                        <input type="text" class="form-control" id="con-answer" name="answer">
                     </div>
-
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="confident" name="confident" value="yes">
-                        <label class="form-check-label" for="confident">I am confident of my answer</label>
-                    </div>
-
-
-
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
                 </div>
