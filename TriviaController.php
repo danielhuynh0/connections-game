4c404a4f1e183725d3e639072cdf1e5e1b4ea383
Daniel Huynh <danielhuynh523@gmail.com>
2023-10-22 16:51:54 -0400

    implemented loading of categories, and choosing the current game's categories to be shown to the user

--- TriviaController.php
+++ TriviaController.php
@@ -2,7 +2,7 @@
 
 class TriviaController {
 
-    private $questions = [];
+    private $categories = [];
 
     private $input = [];
 
@@ -13,38 +13,40 @@ class TriviaController {
         session_start();
         
         $this->input = $input;
-        $this->loadQuestions();
+        $this->loadCategories();
     }
 
     /**
      * Load questions from a URL, store them as an array
      * in the current object.
      */
-    public function loadQuestions() {
-        $this->questions = json_decode(
-            file_get_contents("http://www.cs.virginia.edu/~jh2jf/data/trivia.json"), true);
+    public function loadCategories() {
+        $this->categories = json_decode(
+            file_get_contents("https://www.cs.virginia.edu/~jh2jf/data/categories.json"), true);
 
-        if (empty($this->questions)) {
+        if (empty($this->categories)) {
             die("Something went wrong loading questions");
         }
     }
 
-    /**
-     * Get a question
-     * 
-     * By default, it returns a random question's id and text.  If given
-     * a question id, it returns that question's text and answer.
-     */
-    public function getQuestion($id=null) {
+
+    /*
+        Load list of categories and each of the words in the category to set up the current game board
+    */
+    public function getCategories() {
+
+        $current_game = [];
+
+        foreach ($this->categories as $category) {
+            $cat_name = $category["category"];
+            for ($i = 0; $i < count($category["words"]); $i++) {
+                $cur_word = $category["words"][$i];
+                $temp_list = [$cat_name, $cur_word];
+                array_push($current_game, $temp_list);
+            }
+        }
     
-        if ($id === null) {
-            $id = array_rand($this->questions);
-            return [ "id" => $id, "question" => $this->questions[$id]["question"]];
-        }
-        if (is_numeric($id) && isset($this->questions[$id])) {
-            return $this->questions[$id];
-        }
-        return false;
+        return $current_game;
     }
 
     /**
@@ -64,10 +66,10 @@ class TriviaController {
             case "login":
                 $this->login();
             case "question":
-                $this->showQuestion();
+                $this->showCategories();
                 break;
             case "answer":
-                $this->answerQuestion();
+                $this->submitCategories();
                 break;
             case "logout":
                 $this->logout();
@@ -82,12 +84,13 @@ class TriviaController {
      * template PHP file and displays it to the user based on
      * properties of this object.
      */
-    public function showQuestion($message = "") {
+    public function showCategories($message = "") {
         $name = $_SESSION["name"];
         $email = $_SESSION["email"];
         $score = $_SESSION["score"];
-        $question = $this->getQuestion();
-        include("/opt/src/trivia/templates/question.php");
+        $previous_guesses = $_SESSION["previous_guesses"];
+        $categories = $this->getCategories();
+        include("/opt/src/trivia/templates/board.php");
     }
 
     /**
@@ -100,7 +103,7 @@ class TriviaController {
     /**
      * Check the user's answer to a question.
      */
-    public function answerQuestion() {
+    public function submitCategories() {
         $message = "";
         if (isset($_POST["questionid"]) && is_numeric($_POST["questionid"])) {
             
