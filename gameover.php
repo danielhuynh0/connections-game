3f062f66923b48257630b6e3e63ee05d97d9f828
Daniel Huynh <danielhuynh523@gmail.com>
2023-10-24 16:37:36 -0400

    fixing bug in gameover

--- gameover.php
+++ gameover.php
@@ -32,55 +32,6 @@
             <a href="?command=playagain">Play Again</a>
         </div>
     </div>
-        <div class="row">
-            <div class="col-xs-12">
-                <div class="card">
-                    <div class="card-header">
-                        Words
-                    </div>
-                    <div class="card-body">
-                    <?php
-                        /*$student_one = array("Food"=>"C1", "Food2"=>"C1", "Food3"=>"C1", "Food4"=>"C1",
-                            "Drinks"=>"C2", "Drinks2"=>"C2", "Drinks3"=>"C2", "Drinks4"=>"C2",
-                            "Plate"=>"C3", "Plate2"=>"C3", "Plate3"=>"C3", "Plate4"=>"C3",
-                            "Cup"=>"C4", "Cup2"=>"C4", "Cup3"=>"C4", "Cup4"=>"C4");*/
-                        
-                        $student_one = $current_game;
-
-                        $student_keys = array_keys($student_one);
-                        shuffle($student_keys);
-
-                        $html = "";
-
-                        $count=0;
-
-                        while($count<count($student_one)){
-                            $html .= "<div class=\"row\">\n";
-                            for($x = 0; $x<4; $x++){
-                                $html .= "\t<div class=\"col\">\n \t\t <label>" . $student_keys[$count] . "</label>\n</div>\n";
-                                $count++;
-                            }
-                            $html .= "</div>\n";
-                        }
-
-                        echo $html;
-                    ?>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-xs-12">
-                <form action="?command=answer" method="post">
-                    <div class="mb-3">
-                        <label for="answer" class="form-label">Guess: </label>
-                        <input type="text" class="form-control" id="con-answer" name="answer">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </div>
-        </div>
-    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
