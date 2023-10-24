613051d5818a586197d65153f92b8e926aacdc58
Eric Li <lieric021@gmail.com>
2023-10-24 11:22:07 -0400

    started previous guesses

--- board.php
+++ board.php
@@ -13,9 +13,10 @@
     <div class="container" style="margin-top: 15px;">
         <div class="row">
             <div class="col-xs-12">
-            <h1>PHP Connections</h1>
-            <h4>Hello <?=$name?>! (<?=$email?>)</h4>
-            <h4>Guesses: <?=$score?></h4>
+                <h1>PHP Connections</h1>
+                <h4>Hello <?=$name?>! (<?=$email?>)</h4>
+                <h4>Guesses: <?=$score?></h4>
+                <h4><?=$message?></h4>
             </div>
         </div>
         <div class="row">
@@ -66,6 +67,9 @@
                 </form>
             </div>
         </div>
+        <div style="margin-top: 15px;">
+            <h4>You're previous guesses:</h4>
+        </div>  
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
